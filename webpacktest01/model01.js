65c1266f4908688ccfb8a4a3f219d6e938224547
nereusuaa <nereusua@gmail.com>
2019-07-18 23:34:35 +0800

    2019.7.18 webpack的基本使用

---
+++ webpacktest01/model01.js
@@ -0,0 +1,11 @@
+//定义add函数
+var add = function (x, y) {
+    return x + y
+}
+var add2 = function (x,y) {
+    return x+y+1
+}
+//导出model01.js
+module.exports.add = add;
+// module.exports.add2 = add2;
+// module.exports = {add,add2};
